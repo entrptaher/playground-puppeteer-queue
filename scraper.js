204490c0deb131d3ba3cd5b1fb41b3ed0678bca4
entrptaher@gmail.com <entrptaher@gmail.com>
2019-03-16 14:23:18 +0600

    url fix

--- scraper.js
+++ scraper.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer');
 async function scrape(url){
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+  await page.goto(url);
   const title = await page.title();
   await browser.close();
   return title
